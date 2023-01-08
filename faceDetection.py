31fe43183217541353c1852bc6baf4ba901ea0fa
TarunM <tarun.8792@gmail.com>
2023-01-08 16:35:32 +0530

    add faceDetection.py

---
+++ faceDetection.py
@@ -0,0 +1,22 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+classifier = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+while True:
+    retval, frame = cap.read()
+
+    if retval:
+        faces = classifier.detectMultiScale(frame)
+        for face in faces:
+            x,y,w,h = face
+            out = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 10)
+        cv2.imshow("My Window ", frame)
+
+    key = cv2.waitKey(1)
+    
+    if key == ord("q"):
+        break
+
+cap.realease()
+cap.destoryAllWindows()
